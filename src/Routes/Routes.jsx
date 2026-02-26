92d26d8a5f2163c61062ef2bce0a81c664f4c746
SaPa36 <nursapa2001@gmail.com>
2026-02-26 14:56:48 +0600

    setup the routes

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,28 @@
+
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+    ]
+  },
+]);
+
+const Routes = () => {
+    return (
+        <div> </div>
+    );
+};
+
+export default Routes;
